096bd398df71fd7a1bb9c408e16e4ebc13cc8450
Antonio Chen <chenxuanyi1991@gmail.com>
2017-06-29 21:08:52 +0800

    修改2m-4s-sync配置及启动脚本

---
+++ distribution/bin/run/2m-4s-sync/broker-b.bat
@@ -0,0 +1,2 @@
+cd ../../
+mqbroker.cmd -c ../conf/2m-4s-sync/broker-b.properties -u e:/temp/2m-4s-sync/broker-b -r ../ -n 172.41.6.68:9876
--- distribution/bin/runserver.cmd
+++ distribution/bin/runserver.cmd
@@ -26,7 +26,7 @@ for %%d in (%BASE_DIR%) do set BASE_DIR=%%~dpd
 
 set CLASSPATH=.;%BASE_DIR%conf;%CLASSPATH%
 
-set "JAVA_OPT=%JAVA_OPT% -server -Xms2g -Xmx2g -Xmn1g -XX:PermSize=128m -XX:MaxPermSize=320m"
+set "JAVA_OPT=%JAVA_OPT% -server -Xms1g -Xmx1g -Xmn512m -XX:PermSize=128m -XX:MaxPermSize=320m"
 set "JAVA_OPT=%JAVA_OPT% -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+DisableExplicitGC -XX:-UseParNewGC"
 set "JAVA_OPT=%JAVA_OPT% -verbose:gc -Xloggc:"%USERPROFILE%\rmq_srv_gc.log" -XX:+PrintGCDetails"
 set "JAVA_OPT=%JAVA_OPT% -XX:-OmitStackTraceInFastThrow"
